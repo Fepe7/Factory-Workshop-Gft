d2b3c0853d435bf2bfbc387b60ecbc99128d01d3
Fepe7 <andreistaicu568@gmail.com>
2026-04-30 15:54:19 +0200

    Initial commit

---
+++ src/main/java/com/gft/factoryworkshopgft/FactoryWorkshopGftApplication.java
@@ -0,0 +1,13 @@
+package com.gft.factoryworkshopgft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryWorkshopGftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryWorkshopGftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gft/factoryworkshopgft/TestFactoryWorkshopGftApplication.java
@@ -0,0 +1,11 @@
+package com.gft.factoryworkshopgft;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestFactoryWorkshopGftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(FactoryWorkshopGftApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
