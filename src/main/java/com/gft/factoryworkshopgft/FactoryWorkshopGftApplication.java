b26027e1c7d6e84fa231790e17b2dee66301c68b
Fepe7 <andreistaicu568@gmail.com>
2026-04-30 15:54:57 +0200

    format: adjust indentation in application and test classes

--- src/main/java/com/gft/factoryworkshopgft/FactoryWorkshopGftApplication.java
+++ src/main/java/com/gft/factoryworkshopgft/FactoryWorkshopGftApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FactoryWorkshopGftApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FactoryWorkshopGftApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryWorkshopGftApplication.class, args);
+    }
 
 }
